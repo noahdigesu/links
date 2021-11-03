b9a3698f70cd72bbb0aaa0ae04255a24d3c0c899
Noah Di Gesu <54229@etu.he2b.be>
2021-11-03 11:37:18 +0100

    Moving eyes contained

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,17 +65,35 @@ if (
 		var x = (event.clientX * 200) / window.innerWidth;
 		var y = (event.clientY * 200) / window.innerHeight;
 
-		if (y <= 100) {
+		if (y <= 85 && y >= 15 && x <= 85 && x >= 15) {
 			for (var i = 0; i < 2; i++) {
 				pupil[i].style.left = `${x}%`;
 				pupil[i].style.top = `${y}%`;
 				pupil[i].style.transform = `translate3D(-${x}%, -${y}%, 0)`;
 			}
-		} else {
+		} else if (y > 85) {
 			for (var i = 0; i < 2; i++) {
 				pupil[i].style.left = `${x}%`;
-				pupil[i].style.top = `100%`;
-				pupil[i].style.transform = `translate3D(-${x}%, -100%, 0)`;
+				pupil[i].style.top = `85%`;
+				pupil[i].style.transform = `translate3D(-${x}%, -85%, 0)`;
+			}
+		} else if (y < 15) {
+			for (var i = 0; i < 2; i++) {
+				pupil[i].style.left = `${x}%`;
+				pupil[i].style.top = `15%`;
+				pupil[i].style.transform = `translate3D(-${x}%, -15%, 0)`;
+			}
+		} else if (x > 75) {
+			for (var i = 0; i < 2; i++) {
+				pupil[i].style.left = `75%`;
+				pupil[i].style.top = `${y}%`;
+				pupil[i].style.transform = `translate3D(-75%, -${y}%, 0)`;
+			}
+		} else if (x < 25) {
+			for (var i = 0; i < 2; i++) {
+				pupil[i].style.left = `25%`;
+				pupil[i].style.top = `${y}%`;
+				pupil[i].style.transform = `translate3D(-25%, -${y}%, 0)`;
 			}
 		}
 	};
