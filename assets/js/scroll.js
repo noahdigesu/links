2581cc0fcc17cbfe47b6488c156020f6b68275e1
Noah Di Gesù <noah.digesu@protonmail.com>
2022-11-05 21:56:44 +0100

    Add scroll indicator

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,14 +1,14 @@
 // Scroll progress
-// window.onscroll = function () {
-// 	updateScrollProgress();
-// };
+window.onscroll = function () {
+	updateScrollProgress();
+};
 
-// function updateScrollProgress() {
-// 	var winScroll =
-// 		document.body.scrollLeft || document.documentElement.scrollLeft;
-// 	var width =
-// 		document.documentElement.scrollWidth -
-// 		document.documentElement.clientWidth;
-// 	var scrolled = (winScroll / width) * 100;
-// 	document.getElementById("myBar").style.width = scrolled + "%";
-// }
+function updateScrollProgress() {
+	var winScroll =
+		document.body.scrollLeft || document.documentElement.scrollLeft;
+	var width =
+		document.documentElement.scrollWidth -
+		document.documentElement.clientWidth;
+	var scrolled = (winScroll / width) * 100;
+	document.getElementById("myBar").style.width = scrolled + "%";
+}
