7a153235494497ee2d28b559b0062da5058bb795
Noah Di Gesù <noah.digesu@protonmail.com>
2022-11-05 20:22:12 +0100

    Add cards

---
+++ assets/js/scroll.js
@@ -0,0 +1,14 @@
+// Scroll progress
+// window.onscroll = function () {
+// 	updateScrollProgress();
+// };
+
+// function updateScrollProgress() {
+// 	var winScroll =
+// 		document.body.scrollLeft || document.documentElement.scrollLeft;
+// 	var width =
+// 		document.documentElement.scrollWidth -
+// 		document.documentElement.clientWidth;
+// 	var scrolled = (winScroll / width) * 100;
+// 	document.getElementById("myBar").style.width = scrolled + "%";
+// }
