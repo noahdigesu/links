83a12936a09081b06c09bdc6a75236b1d60f5ca4
Noah Di Gesù <noah.digesu@protonmail.com>
2022-11-08 14:40:18 +0100

    Add alt animation

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,7 +1,12 @@
 const cards = document.querySelectorAll(".card");
+let magicLines = document.querySelectorAll(".magic-line");
+magicLines = [...magicLines];
 
 [...cards].forEach(function (card, i) {
+	setTimeout(function () {
+		card.classList.add("slide-in-top");
+	}, i * 500);
     setTimeout(function () {
-        card.classList.add("slide-in-top");
-    }, i * 200);
+        magicLines[i].classList.add("disappear");
+    }, i * 500 + 1500);
 });
