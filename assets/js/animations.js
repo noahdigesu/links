78f3a7790f4786128243cd19d20fc29f171c6db6
Noah Di Gesù <noah.digesu@protonmail.com>
2022-11-08 17:58:50 +0100

    Sweet animations

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,12 +1,24 @@
 const cards = document.querySelectorAll(".card");
-let magicLines = document.querySelectorAll(".magic-line");
-magicLines = [...magicLines];
+const magicLines = document.querySelectorAll(".magic-line");
+const marquee = document.querySelector(".marquee");
+
+[...magicLines].forEach(function (line, i) {
+    setTimeout(function () {
+		line.classList.add("appear");
+	}, i * 500);
+});
 
 [...cards].forEach(function (card, i) {
 	setTimeout(function () {
 		card.classList.add("slide-in-top");
-	}, i * 500);
-    setTimeout(function () {
-        magicLines[i].classList.add("disappear");
-    }, i * 500 + 1500);
+	}, i * 500 + 500);
+
+	setTimeout(function () {
+		[...magicLines][i].style.right = 0;
+		[...magicLines][i].classList.add("disappear");
+	}, i * 500 + 500 + 1500);
+
+    setTimeout(() => {
+        marquee.style.animationPlayState = "running";
+    }, 500 + 500 + 1500 + [...cards].length);
 });
