8bf445f775c1eae459b03b734d2b3c060fd77cd9
Noah Di Gesù <noah.digesu@protonmail.com>
2022-11-10 15:05:59 +0100

    Refactor & remove marquee animations on mobile

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,24 +1,30 @@
-// Marquee
+// Marquee (desktop-only)
 
-const marquee = document.querySelector(".marquee");
-const marqueeLetters = document.querySelectorAll(".stroke path");
+let desktop = false;
+if (!window.matchMedia("(max-width: 640px)").matches) {
+	desktop = true;
+	const marquee = document.querySelector(".marquee");
+	const marqueeLetters = document.querySelectorAll(".stroke path");
 
-[...marqueeLetters].forEach(function (letter, i) {
-	setTimeout(function () {
-		letter.classList.add("stroke-in");
-	}, i * 300);
+	[...marqueeLetters].forEach(function (letter, i) {
+		setTimeout(function () {
+			letter.classList.add("stroke-in");
+		}, i * 300);
 
-    setTimeout(() => {
-        marquee.style.animationPlayState = "running";
-    }, (300 * 5) + 3000);
-});
+		setTimeout(() => {
+			marquee.style.animationPlayState = "running";
+		}, 300 * 5 + 3000);
+	});
+}
 
 // Cards
+delay = desktop ? 300 + 500 * 5 : 0;
+
 setTimeout(function () {
-    const cards = document.querySelectorAll(".card");
+	const cards = document.querySelectorAll(".card");
 	const magicLines = document.querySelectorAll(".magic-line");
 
-    [...magicLines].forEach(function (line, i) {
+	[...magicLines].forEach(function (line, i) {
 		setTimeout(function () {
 			line.classList.add("appear");
 		}, i * 500);
@@ -34,4 +40,4 @@ setTimeout(function () {
 			[...magicLines][i].classList.add("disappear");
 		}, i * 500 + 500 + 1500);
 	});
-}, 300 + 500 * 5);
+}, delay);
